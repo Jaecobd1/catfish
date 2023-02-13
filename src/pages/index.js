5df34dde6750858a11dd0097c8c7722907313a33
Jaecobd1 <jaecob.d1@gmail.com>
2023-02-13 16:34:28 -0500

    fixing styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="main">
+      <main className={styles.main}>
         <p>hello, this is a test</p>
       </main>
     </>
