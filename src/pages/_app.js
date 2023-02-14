903f0f141e7ac7bc938263126662aa31b36a9bd0
Jaecobd1 <jaecob.d1@gmail.com>
2023-02-13 19:15:08 -0500

    sync

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,12 @@ import "@/styles/globals.css";
 import { UserContext } from "@/lib/context";
 import Nav from "@/components/Nav/Nav";
 import { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function App({ Component, pageProps }) {
   return (
-    <UserContext.Provider value={{ user: {}, username: {} }}>
+    <UserContext.Provider value={{ user: {}, username: "Jake" }}>
       <Nav />
       <Component {...pageProps} />
       <Toaster />
