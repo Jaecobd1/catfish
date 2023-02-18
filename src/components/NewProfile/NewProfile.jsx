0b405b7add0ffb941aa0bf2ed8a4437a90996b4e
Jaecobd1 <jaecob.d1@gmail.com>
2023-02-18 18:06:57 -0500

    moving username to newProfile

--- src/components/NewProfile/NewProfile.jsx
+++ src/components/NewProfile/NewProfile.jsx
@@ -1,1 +1,112 @@
-// everything relating to the username function 
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "@/lib/context";
+import { useCallback } from "react";
+import debounce from "lodash.debounce";
+import { firestore } from "../../lib/firebase";
+// everything relating to the username function
+// Username Validation form
+export function UsernameForm() {
+  const [formValue, setFormValue] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const { user, username } = useContext(UserContext);
+
+  useEffect(() => {
+    checkUsername(formValue);
+    console.log(user.displayName);
+  }, [formValue]);
+
+  // Regex
+  const onChange = (e) => {
+    const val = e.target.value.toLowerCase();
+    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    // Only set form calue if length is < 3 Or it passes regex
+    if (val.length < 3) {
+      setFormValue(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if (re.test(val)) {
+      setFormValue(val);
+      setLoading(true);
+      setIsValid(true);
+    }
+  };
+
+  // Check database for username match after debounce
+  // useCallback is needed for debounce
+  const checkUsername = useCallback(
+    debounce(async (username) => {
+      if (username.length >= 3) {
+        const ref = firestore.doc(`usernames/${username}`);
+        const { exists } = await ref.get();
+        setIsValid(!exists);
+        setLoading(false);
+      }
+    }, 500),
+    []
+  );
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const userDoc = firestore.doc(`users/${user.uid}`);
+    const usernameDoc = firestore.doc(`usernames/${formValue}`);
+
+    // Send to DB at same time
+    const batch = firestore.batch();
+    batch.set(userDoc, {
+      username: formValue,
+      photoURL: "",
+      displayName: formValue,
+      firstName: "",
+      lastName: "",
+      isUserInGame: false,
+    });
+    batch.set(usernameDoc, { uid: user.uid });
+
+    await batch.commit();
+  };
+
+  return (
+    !username && (
+      <>
+        <section>
+          <h3>Username</h3>
+          <form onSubmit={onSubmit} className="flex flex-col">
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              value={formValue}
+              onChange={onChange}
+            />
+            <UsernameMessage
+              username={formValue}
+              isValid={isValid}
+              loading={loading}
+            />
+            <button type="submit" disabled={!isValid}>
+              Choose
+            </button>
+          </form>
+        </section>
+      </>
+    )
+  );
+}
+
+// Shows the user if the username is available
+function UsernameMessage({ username, isValid, loading }) {
+  if (loading) {
+    return <p> Checking...</p>;
+  } else if (isValid) {
+    return <p>{username} is available!</p>;
+  } else if (!isValid) {
+    return <p>{username} is not available</p>;
+  } else {
+    <p></p>;
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,7 @@ import Game from "@/components/Game/Game";
 import Hero from "@/components/Home/Hero";
 import { FaArrowCircleRight } from "react-icons/fa";
 import Image from "next/image";
+import { UsernameForm } from "../components/NewProfile/NewProfile";
 
 const inter = Inter({ subsets: ["latin"] });
 const lato = Lato({
@@ -123,108 +124,4 @@ export default function Home() {
       </div>
     </>
   );
-}
-
-// Username Validation form
-function UsernameForm() {
-  const [formValue, setFormValue] = useState("");
-  const [isValid, setIsValid] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const { user, username } = useContext(UserContext);
-
-  useEffect(() => {
-    checkUsername(formValue);
-    console.log(user.displayName);
-  }, [formValue]);
-
-  // Regex
-  const onChange = (e) => {
-    const val = e.target.value.toLowerCase();
-    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
-
-    // Only set form calue if length is < 3 Or it passes regex
-    if (val.length < 3) {
-      setFormValue(val);
-      setLoading(false);
-      setIsValid(false);
-    }
-
-    if (re.test(val)) {
-      setFormValue(val);
-      setLoading(true);
-      setIsValid(true);
-    }
-  };
-
-  // Check database for username match after debounce
-  // useCallback is needed for debounce
-  const checkUsername = useCallback(
-    debounce(async (username) => {
-      if (username.length >= 3) {
-        const ref = firestore.doc(`usernames/${username}`);
-        const { exists } = await ref.get();
-        setIsValid(!exists);
-        setLoading(false);
-      }
-    }, 500),
-    []
-  );
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-
-    const userDoc = firestore.doc(`users/${user.uid}`);
-    const usernameDoc = firestore.doc(`usernames/${formValue}`);
-
-    // Send to DB at same time
-    const batch = firestore.batch();
-    batch.set(userDoc, {
-      username: formValue,
-      photoURL: "",
-      displayName: formValue,
-      firstName: "",
-      lastName: "",
-      isUserInGame: false,
-    });
-    batch.set(usernameDoc, { uid: user.uid });
-
-    await batch.commit();
-  };
-
-  return (
-    !username && (
-      <>
-        <section>
-          <h3>Username</h3>
-          <form onSubmit={onSubmit} className="flex flex-col">
-            <input
-              type="text"
-              name="username"
-              placeholder="username"
-              value={formValue}
-              onChange={onChange}
-            />
-            <UsernameMessage
-              username={formValue}
-              isValid={isValid}
-              loading={loading}
-            />
-            <button type="submit" disabled={!isValid}>
-              Choose
-            </button>
-          </form>
-        </section>
-      </>
-    )
-  );
-}
-
-function UsernameMessage({ username, isValid, loading }) {
-  if (loading) {
-    return <p> Checking...</p>;
-  } else if (isValid) {
-    return <p>{username} is available!</p>;
-  } else {
-    return <p></p>;
-  }
 }
