d83dc1c76d8d3c7ac55d6267b001387b486056be
Jaecobd1 <jaecob.d1@gmail.com>
2023-02-19 23:17:20 -0500

    username

--- src/components/Game/Chat.jsx
+++ src/components/Game/Chat.jsx
@@ -100,7 +100,7 @@ function ChatMessage(props) {
             />
           </div>
           <div className={styles.textContent}>
-          <p className="mb-2 font-raleway font-bold">Username</p>
+            <p className="mb-2 font-raleway font-bold">{username}</p>
             <p className="text">{text}</p>
           </div>
         </div>
